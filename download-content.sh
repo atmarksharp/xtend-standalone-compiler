aadbf8ea35a558525d6f8a163c06eacda4597e04
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2014-09-17 23:03:08 +0900

    Update files

--- download-content.sh
+++ download-content.sh
@@ -1,8 +1,3 @@
-wget https://github.com/atmarksharp/xtend-standalone-compiler/archive/master.zip
-unzip -xq master.zip
-rm -rf master.zip
-cd xtend-standalone-compiler
-
 wget https://github.com/atmarksharp/atmarksharp.github.io/raw/master/xtend-standalone-compiler/xtend-libraries.zip
 unzip -xq xtend-libraries.zip
 rm -rf xtend-libraries.zip
---
+++ download-dir.sh
@@ -0,0 +1,3 @@
+wget https://github.com/atmarksharp/xtend-standalone-compiler/archive/master.zip
+unzip -xq master.zip
+rm -rf master.zip
